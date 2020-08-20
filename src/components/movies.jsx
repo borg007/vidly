86772f83f04ca23e9abafe4a607a41fbb3b20526
borg007 <marlonben@gmail.com>
2020-08-19 20:21:40 -0400

    Conditional Rendering 1

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,8 +6,19 @@ class Movies extends Component {
     movies: getMovies()
   };
 
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
   render() {
-    return (
+      const { length: count} = this.state.movies;
+
+    if (this.state.movies.length === 0)
+      return <p>There are no movies in the database!</p>;
+    
+      return (
+      <p>Showing {this.state.movies.length} movies in the database.</p>
       <table className="table">
         <thead>
           <tr>
@@ -15,15 +26,24 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
