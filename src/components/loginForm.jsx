4c6ab12ca6c8ef39c1c3f2d5ffa4bd02a700df0e
borg007 <marlonben@gmail.com>
2020-08-27 17:34:42 -0400

    Getting input value from the React Dom using Ref

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef(); // only use refs to access the react dom if you have to
+
+  /* componentDidMount() {
+    this.username.current.focus(); // causes username input field to appear active or focused
+  }*/
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // Call the server
+
+    const username = this.username.current.value;
+  };
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              maxLength="20"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              id="password"
+              type="text"
+              maxLength="20"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
