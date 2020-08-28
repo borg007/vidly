60ceed676a1f1f788176aeed04d970539e578d99
borg007 <marlonben@gmail.com>
2020-08-27 21:22:41 -0400

    Handling Multiple inputs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -13,7 +13,15 @@ class LoginForm extends Component {
 
     const username = this.username.current.value;
   };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -21,23 +29,29 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              autoFocus
-              ref={this.username}
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               maxLength="20"
               className="form-control"
             />
           </div>
+
           <div className="form-group">
             <label htmlFor="password">Password</label>
             <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
               id="password"
               type="text"
               maxLength="20"
               className="form-control"
             />
           </div>
+
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
