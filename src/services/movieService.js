d702c1786e257bf922b928f12d9a2c237e10e1e8
borg007 <marlonben@gmail.com>
2020-09-27 20:49:58 -0400

    Replacing FakeMovieService r2

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,8 +1,10 @@
 import http from "./httpService";
 
+const apiEndpoint = "http://localhost:3900/api/movies";
+
 export function getMovies() {
-  return http.get("http://localhost:3900/api/movies");
+  return http.get(apiEndpoint);
 }
 export function deleteMovie(movieId) {
-  return http.delete("http://localhost:3900/api/movies" + "/" + movieId);
+  return http.delete(apiEndpoint + "/" + movieId);
 }
